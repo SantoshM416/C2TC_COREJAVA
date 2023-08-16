07f62d4b848926ddb455393483a75848d8e75a7a
Ayan Santi <Ayan Santi@DESKTOP-91CAOSD>
2023-08-16 12:44:17 +0530

    FIRST COMMIT

---
+++ SantoshYadawad/src/Demochar.java
@@ -0,0 +1,16 @@
+
+public class Demochar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char ch='a';
+		System.out.println(ch);
+		char chone=78;
+		System.out.println(chone);
+		char chtwo='\u20AC';
+		System.out.println(chtwo);
+		
+	}
+
+}
+
---
+++ SantoshYadawad/src/OperatorsDemo.java
@@ -0,0 +1,10 @@
+
+public class OperatorsDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int b=15,c=25, a=10;
+		System.out.println(a + c + c*b);
+	}
+
+}
