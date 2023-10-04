b67dbeb50a5a2feb68f9937c8a8b07f7eb97a2be
Kalmesh <Kalmesh@DESKTOP-G1MFF4M>
2023-10-04 21:40:45 +0530

    first commit

---
+++ DayTwelve/src/com/cg/nestedtryfinally/NestedTryDemo.java
@@ -0,0 +1,10 @@
+package com.cg.nestedtryfinally;
+
+public class NestedTryDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		NestedTry.evaluate();
+	}
+
+}
