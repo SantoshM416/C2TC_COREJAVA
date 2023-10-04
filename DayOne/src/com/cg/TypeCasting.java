e84a71fafa018e9d7738c6804210aac6884c2c6d
Kalmesh <Kalmesh@DESKTOP-G1MFF4M>
2023-10-04 11:53:59 +0530

    first commit

---
+++ DayOne/src/com/cg/TypeCasting.java
@@ -0,0 +1,13 @@
+package com.cg;
+
+public class TypeCasting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double d=9.78d;
+	     int i=(int)d;
+	     System.out.println(d);
+	     System.out.println(i);
+	}
+
+}
--- DayOne/src/module-info.java
+++ DayOne/src/module-info.java
@@ -4,5 +4,5 @@
 /**
  * 
  */
-module hello {
+module DayOne {
 }
