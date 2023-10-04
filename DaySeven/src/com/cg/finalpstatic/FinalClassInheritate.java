89ca07c2af0d0f060fceb6b88c1c5778ae57c0bc
Kalmesh <Kalmesh@DESKTOP-G1MFF4M>
2023-10-04 20:21:22 +0530

    first commit

--- DaySeven/src/com/cg/finalpstatic/FinalClassInheritate.java
+++ DaySeven/src/com/cg/finalpstatic/FinalClassInheritate.java
@@ -1,5 +1,10 @@
 package com.cg.finalpstatic;
 
 public class FinalClassInheritate extends FinalWithClass {
+
+	public FinalClassInheritate(int y) {
+		super(y);
+		// TODO Auto-generated constructor stub
+	}
  
 }
