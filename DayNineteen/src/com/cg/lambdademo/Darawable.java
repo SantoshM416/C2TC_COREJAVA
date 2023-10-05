2f64412990de0212a433cec57c3b6b407e5d9511
Kalmesh <Kalmesh@DESKTOP-G1MFF4M>
2023-10-05 12:54:54 +0530

    first commit

---
+++ DayNineteen/src/com/cg/lambdademo/Darawable.java
@@ -0,0 +1,5 @@
+package com.cg.lambdademo;
+
+public interface Darawable {
+	void draw();
+}
---
+++ DayNineteen/src/com/cg/lambdademo/WithoutLambdaExpression.java
@@ -0,0 +1,21 @@
+package com.cg.lambdademo;
+
+public class WithoutLambdaExpression implements Darawable{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Darawable d1 = new Darawable() {
+			public void draw() {
+				System.out.println("Draw");
+			}
+		};
+        d1.draw();
+	}
+
+	@Override
+	public void draw() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
