c4f59371d60ab900c4179701db36aa9c4dd27724
Kalmesh <Kalmesh@DESKTOP-G1MFF4M>
2023-10-05 12:32:05 +0530

    first commit

---
+++ DayEighteen/src/com/cg/deqeue/PriorityQueueDemo.java
@@ -0,0 +1,25 @@
+package com.cg.deqeue;
+import java.util.PriorityQueue;
+
+public class PriorityQueueDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		PriorityQueue<String> prq = new PriorityQueue<>();
+		prq.add("Ashish");
+		prq.add("Patna");
+		prq.add("Hyderabad");
+		prq.add("Banglore");
+		//prq.add(null);
+		prq.add("Saharsa");
+		prq.add("Madhepura");
+		System.out.println(prq);
+		System.out.println(prq.remove());
+		System.out.println(prq.size());
+		System.out.println(prq.equals("Saharsa"));
+
+
+	}
+
+}
