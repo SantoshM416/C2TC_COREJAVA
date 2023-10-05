7004b6c2ec377ac987860966cb8262c089c3df32
Kalmesh <Kalmesh@DESKTOP-G1MFF4M>
2023-10-05 11:28:41 +0530

    first commit

---
+++ DayFifteen/src/com/cg/genericillustration/LowerBoundedWildCard.java
@@ -0,0 +1,12 @@
+package com.cg.genericillustration;
+import java.util.List;
+
+public class LowerBoundedWildCard {
+
+	public static void display(List<? super Integer> l) {
+		for(Object o:l) {
+			System.out.println(o);
+		}
+		
+	}
+}
---
+++ DayFifteen/src/com/cg/genericillustration/UnBoundedWildcard.java
@@ -0,0 +1,13 @@
+package com.cg.genericillustration;
+
+import java.util.List;
+
+public class UnBoundedWildcard {
+	
+	public static void display(List<?>  arr) {
+		for(Object o:arr) {
+			System.out.println(o);
+		}
+	}
+
+}
