2ac038f1743dfb5a52aecdfd18bb527d3a962133
Kalmesh <Kalmesh@DESKTOP-G1MFF4M>
2023-10-05 11:47:40 +0530

    first commit

---
+++ DaySixteen/src/com/cg/comparatordemo/SortByRollNumber.java
@@ -0,0 +1,12 @@
+package com.cg.comparatordemo;
+
+import java.util.Comparator;
+
+public class SortByRollNumber  implements Comparator<Student>{
+
+	@Override
+	public int compare(Student s1, Student s2) {
+		// TODO Auto-generated method stub
+		return s1.rollNumber-s2.rollNumber;
+	}
+}
